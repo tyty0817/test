e7f4f437c0c101ab810282c03c54dc003f015bd9
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:45:25 -0700

    Create guitar.lua

---
+++ lua/guitar.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/guitar.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "guitar.dfpwm")
+shell.run("rom/programs/delete", "guitar.dfpwm")
