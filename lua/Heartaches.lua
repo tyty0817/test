04e8ae07ea07ee685c643e9d964bfe7a03a42467
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:42:49 -0700

    Create Heartaches.lua

---
+++ lua/Heartaches.lua
@@ -0,0 +1,6 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/Heartaches1.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "Heartaches1.dfpwm")
+shell.run("rom/programs/delete", "Heartaches1.dfpwm")
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/Heartaches2.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "Heartaches2.dfpwm")
+shell.run("rom/programs/delete", "Heartaches2.dfpwm")
