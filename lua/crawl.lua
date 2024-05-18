9ef3f0f167855f19555784c2a75d3b0c82c63ffa
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-18 01:04:20 -0700

    Create crawl.lua

---
+++ lua/crawl.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/crawl.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "crawl.dfpwm")
+shell.run("rom/programs/delete", "crawl.dfpwm")
