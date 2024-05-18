4f2ae16c4354fae1fa31754ca7d6572e7480434a
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:44:45 -0700

    Create cry.lua

---
+++ lua/cry.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/cry.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "cry.dfpwm")
+shell.run("rom/programs/delete", "cry.dfpwm")
