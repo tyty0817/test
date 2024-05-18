aba42e09101ee0cb9fb14c3e68d3f0dfb539ab66
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-18 01:03:35 -0700

    Create uranium.lua

---
+++ lua/uranium.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/uranium.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "uranium.dfpwm")
+shell.run("rom/programs/delete", "uranium.dfpwm")
