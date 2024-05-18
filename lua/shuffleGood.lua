126b554ddf829cc9187c42a19018e00f71292dc3
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-18 00:04:50 -0700

    Update shuffleGood.lua

--- lua/shuffleGood.lua
+++ lua/shuffleGood.lua
@@ -3,23 +3,25 @@ do
   x = math.random(11)
   if x == 1 then
     shell.run("16.lua")
-  if x == 2 then
+  elseif x == 2 then
     shell.run("act.lua")
-  if x == 3 then
+  elseif x == 3 then
     shell.run("cry.lua")
-  if x == 4 then
+  elseif x == 4 then
     shell.run("guitar.lua")
-  if x == 5 then
+  elseif x == 5 then
     shell.run("lonesome.lua")
-  if x == 6 then
+  elseif x == 6 then
     shell.run("Sin.lua")
-  if x == 7 then
+  elseif x == 7 then
     shell.run("Heartache.lua")
-  if x == 8 then
+  elseif x == 8 then
     shell.run("Dixie.lua")
-  if x == 9 then
+  elseif x == 9 then
     shell.run("iron.lua")
-  if x == 10 then
+  elseif x == 10 then
     shell.run("shadow.lua")
-  if x == 11 then
+  elseif x == 11 then
     shell.run("stars.lua")
+                      end
+                    end
