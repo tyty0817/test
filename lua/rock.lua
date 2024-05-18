807e53e0ef1dcf30a926bab143182f962e31c086
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-18 01:03:56 -0700

    Create rock.lua

---
+++ lua/rock.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/rock.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "rock.dfpwm")
+shell.run("rom/programs/delete", "rock.dfpwm")
