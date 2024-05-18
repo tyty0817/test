fb123dc0f4c69908e71dd78f3c8366a4da9c52b1
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-18 00:16:46 -0700

    Create kick.lua

---
+++ lua/kick.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/kick.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "kick.dfpwm")
+shell.run("rom/programs/delete", "kick.dfpwm")
