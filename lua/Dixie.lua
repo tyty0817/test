c312e255a7803c2f8bb3ac185966336d882ac238
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:41:52 -0700

    Create Dixie.lua

---
+++ lua/Dixie.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/Dixie.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "Dixie.dfpwm")
+shell.run("rom/programs/delete", "Dixie.dfpwm")
