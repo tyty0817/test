99ec4515da5ec0d4a0500be3434bacec8905c4d6
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:49:27 -0700

    Create shadow.lua

---
+++ lua/shadow.lua
@@ -0,0 +1,6 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/shadow1.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "shadow1.dfpwm")
+shell.run("rom/programs/delete", "shadow1.dfpwm")
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/shadow2.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "shadow2.dfpwm")
+shell.run("rom/programs/delete", "shadow2.dfpwm")
