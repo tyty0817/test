ad76f00f3588ce4071f0f758cad7dcc9bb501eea
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:43:47 -0700

    Create Sin.lua

---
+++ lua/Sin.lua
@@ -0,0 +1,3 @@
+shell.run("rom/programs/http/wget", "https://github.com/tyty0817/test/raw/main/Sin.dfpwm")
+shell.run("rom/programs/fun/speaker", "play", "Sin.dfpwm")
+shell.run("rom/programs/delete", "Sin.dfpwm")
