284d0597938e85b152f869b46d05e128175bdb20
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-17 23:52:24 -0700

    Create shuffleGood.lua

---
+++ lua/shuffleGood.lua
@@ -0,0 +1,25 @@
+while(true)
+do
+  x = math.random(11)
+  if x == 1 then
+    shell.run("16.lua")
+  if x == 2 then
+    shell.run("act.lua")
+  if x == 3 then
+    shell.run("cry.lua")
+  if x == 4 then
+    shell.run("guitar.lua")
+  if x == 5 then
+    shell.run("lonesome.lua")
+  if x == 6 then
+    shell.run("Sin.lua")
+  if x == 7 then
+    shell.run("Heartache.lua")
+  if x == 8 then
+    shell.run("Dixie.lua")
+  if x == 9 then
+    shell.run("iron.lua")
+  if x == 10 then
+    shell.run("shadow.lua")
+  if x == 11 then
+    shell.run("stars.lua")
