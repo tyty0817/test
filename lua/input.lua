fefe7992297eeaf530aba5f301f8af196c65d767
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-05-21 03:58:43 -0700

    Update input.lua

--- lua/input.lua
+++ lua/input.lua
@@ -3,29 +3,29 @@ modem.open(42)
 repeat
     event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     if message == 16 then
-    shell.run("16.lua")
-        elseif message == "act" then
-    shell.run("act.lua")
-        elseif message == "cry" then
-    shell.run("cry.lua")
-        elseif message == "guitar" then
-    shell.run("guitar.lua")
-        elseif message == "lonesome" then
-    shell.run("lonesome.lua")
-        elseif message == "Sin" then
-    shell.run("Sin.lua")
-        elseif message == "Heartache" then
-    shell.run("Heartache.lua")
-        elseif message == "Dixie" then
-    shell.run("Dixie.lua")
-        elseif message == "iron" then
-    shell.run("iron.lua")
-        elseif message == "shadow" then
-    shell.run("shadow.lua")
-        elseif message == "stars" then
-    shell.run("stars.lua")
-        elseif message == "water" then
-    shell.run("water.lua")
+        shell.run("16.lua")
+    elseif message == "act" then
+        shell.run("act.lua")
+    elseif message == "cry" then
+        shell.run("cry.lua")
+    elseif message == "guitar" then
+        shell.run("guitar.lua")
+    elseif message == "lonesome" then
+        shell.run("lonesome.lua")
+    elseif message == "Sin" then
+        shell.run("Sin.lua")
+    elseif message == "Heartache" then
+        shell.run("Heartache.lua")
+    elseif message == "Dixie" then
+        shell.run("Dixie.lua")
+    elseif message == "iron" then
+        shell.run("iron.lua")
+    elseif message == "shadow" then
+        shell.run("shadow.lua")
+    elseif message == "stars" then
+        shell.run("stars.lua")
+    elseif message == "water" then
+        shell.run("water.lua")
     elseif message == "kick" then
         shell.run("kick.lua")
     elseif message == "rock" then
