2d789216e2b24430aaa503237693e6e3ed466f61
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-12-06 01:43:57 -0800

    Update shuffle.lua

--- lua/shuffle.lua
+++ lua/shuffle.lua
@@ -1,36 +1,39 @@
-x = math.random(17)
-if x == 17 then
-    shell.run("16.lua")
-elseif x == 16 then
-    shell.run("act.lua")
-elseif x == 15 then
-    shell.run("cry.lua")
-elseif x == 14 then
-    shell.run("guitar.lua")
-elseif x == 13 then
-    shell.run("lonesome.lua")
-elseif x == 12 then
-    shell.run("Sin.lua")
-elseif x == 11 then
-    shell.run("Heartache.lua")
-elseif x == 10 then
-    shell.run("Dixie.lua")
-elseif x == 9 then
-    shell.run("iron.lua")
-elseif x == 8 then
-    shell.run("shadow.lua")
-elseif x == 7 then
-    shell.run("stars.lua")
-elseif x == 6 then
-    shell.run("water.lua")
-elseif x == 5 then
-    shell.run("kick.lua")
-elseif x == 4 then
-    shell.run("rock.lua")
-elseif x == 3 then
-    shell.run("uranium.lua")
-elseif x == 2 then
-    shell.run("crawl.lua")
-elseif x == 1 then
-    shell.run("bongo.lua")
-goto start
+x = 0
+while(true)
+do
+    x = math.random(17)
+    if x == 17 then
+        shell.run("16.lua")
+    elseif x == 16 then
+        shell.run("act.lua")
+    elseif x == 15 then
+        shell.run("cry.lua")
+    elseif x == 14 then
+        shell.run("guitar.lua")
+    elseif x == 13 then
+        shell.run("lonesome.lua")
+    elseif x == 12 then
+        shell.run("Sin.lua")
+    elseif x == 11 then
+        shell.run("Heartache.lua")
+    elseif x == 10 then
+        shell.run("Dixie.lua")
+    elseif x == 9 then
+        shell.run("iron.lua")
+    elseif x == 8 then
+        shell.run("shadow.lua")
+    elseif x == 7 then
+        shell.run("stars.lua")
+    elseif x == 6 then
+        shell.run("water.lua")
+    elseif x == 5 then
+        shell.run("kick.lua")
+    elseif x == 4 then
+        shell.run("rock.lua")
+    elseif x == 3 then
+        shell.run("uranium.lua")
+    elseif x == 2 then
+        shell.run("crawl.lua")
+    elseif x == 1 then
+        shell.run("bongo.lua")
+end
