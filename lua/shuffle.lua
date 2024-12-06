82d1ea277adeca0ebcd2c561f4591fde1a106167
tyty0817 <69058811+tyty0817@users.noreply.github.com>
2024-12-06 01:27:26 -0800

    Add files via upload

---
+++ lua/shuffle.lua
@@ -0,0 +1,37 @@
+while(true){
+    x = math.random(17)
+    if x == 16 then
+        shell.run("16.lua")
+    elseif x == "act" then
+        shell.run("act.lua")
+    elseif x == "cry" then
+        shell.run("cry.lua")
+    elseif x == "guitar" then
+        shell.run("guitar.lua")
+    elseif x == "lonesome" then
+        shell.run("lonesome.lua")
+    elseif x == "Sin" then
+        shell.run("Sin.lua")
+    elseif x == "Heartache" then
+        shell.run("Heartache.lua")
+    elseif x == "Dixie" then
+        shell.run("Dixie.lua")
+    elseif x == "iron" then
+        shell.run("iron.lua")
+    elseif x == "shadow" then
+        shell.run("shadow.lua")
+    elseif x == "stars" then
+        shell.run("stars.lua")
+    elseif x == "water" then
+        shell.run("water.lua")
+    elseif x == "kick" then
+        shell.run("kick.lua")
+    elseif x == "rock" then
+        shell.run("rock.lua")
+    elseif x == "uranium" then
+        shell.run("uranium.lua")
+    elseif x == "crawl" then
+        shell.run("crawl.lua")
+    elseif x == "bongo" then
+        shell.run("bongo.lua")
+    }
